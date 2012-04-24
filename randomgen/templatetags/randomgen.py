4e9cbb66dcd9484d42c3965ebce875e28b75eae9
Ben Keating <ben@deepdream.com>
2012-04-24 08:50:49 -0700

    Added initial tag code, taking directly from the django docs. Currently broken
    {% randomgen %} should, in it's current state, return a
    TemplateSyntaxError but instead is not even being registered as a
    template tag.

---
+++ randomgen/templatetags/randomgen.py
@@ -0,0 +1,24 @@
+import datetime
+import random
+import hashlib
+
+from django import template
+
+register = template.Library()
+
+class CurrentTimeNode(template.Node):
+    def __init__(self, format_string):
+        self.format_string = format_string
+    def render(self, context):
+        return datetime.datetime.now().strftime(self.format_string)
+        
+@register.tag(name="randomgen")
+def do_current_time(parser, token):
+    try:
+        # split_contents() knows not to split quoted strings.
+        tag_name, format_string = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError("%r tag requires a single argument" % token.contents.split()[0])
+    if not (format_string[0] == format_string[-1] and format_string[0] in ('"', "'")):
+        raise template.TemplateSyntaxError("%r tag's argument should be in quotes" % tag_name)
+    return CurrentTimeNode(format_string[1:-1])
